9bbe72bb8decc660979676dbe1183f656b11f4f6
Andrew Wolstenholme <ajwsail99@gmail.com>
2021-07-30 17:00:27 -0400

    post kevin work

--- src/Components/PlaySound.js
+++ src/Components/PlaySound.js
@@ -1,35 +1,13 @@
 import Sound from 'react-sound';
 import React, {useState, useEffect} from 'react';
-import Grid from '@material-ui/core/Grid';
-import Slider from '@material-ui/core/Slider';
-import VolumeDown from '@material-ui/icons/VolumeDown';
-import VolumeUp from '@material-ui/icons/VolumeUp';
 
 function PlaySound(props) {
     
     const [isPlaying, setIsPlaying] = useState(false);
-    const [value, setValue] = useState(0);
 
     const handleChange = (event, newValue) => {
-        setValue(newValue);
         setIsPlaying(true)
     };
-
-    // // as component did mount
-    // useEffect(()=>{
-    //    setIsPlaying(!isPlaying)
-    // },[props.setIntitialPlay]);
-
-    // //as component did update
-    // useEffect(()=>{
-    //     setValue(Math.random() * 100)
-    // },[props.randomize]);
-    
-    // how to only call this when the button is pressed
-    // //as component did update
-    // useEffect(()=>{
-    //     setValue(0)
-    // },[props.mute]);
 
     return (
         <div>
@@ -41,22 +19,12 @@ function PlaySound(props) {
                 url={props.song.path}
                 playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
                 playFromPosition={10}
-                volume={value}
+                volume={props.volume}
                 loop = {true}
                 //autoLoad={true}
             />
             
-            <Grid container spacing={2}>
-                <Grid item>
-                    <VolumeDown />
-                </Grid>
-                <Grid item xs>
-                    <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
-                </Grid>
-                <Grid item>
-                    <VolumeUp />
-                </Grid>
-            </Grid>
+    
         </div>
     );
 } export default PlaySound;
--- src/Components/SoundWrapper.js
+++ src/Components/SoundWrapper.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import PlaySound from './PlaySound'
+import Sound from 'react-sound';
+import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/core/Slider';
+import VolumeDown from '@material-ui/icons/VolumeDown';
+import VolumeUp from '@material-ui/icons/VolumeUp';
 export default class SoundWrapper extends Component {
 
     constructor(props){
@@ -7,12 +12,22 @@ export default class SoundWrapper extends Component {
         this.state={
             // is muted should be true while is playing is false on start
             setIntitialPlay:false,
-            randomize:false,
-            mute:false,
+            playSounds:false,
+            isPlaying:false,
+            
         }
         this.initHandler = this.initHandler.bind(this)
-        this.randomize = this.randomize.bind(this)
+        this.playSounds = this.playSounds.bind(this)
         this.mute = this.mute.bind(this)
+        this.handleVolume = this.handleVolume.bind(this)
+    }
+
+    componentDidMount(){
+        this.props.songs.map((song) =>{
+            this.setState({
+                [song.title]: 0
+            })
+        })
     }
 
     initHandler() {
@@ -21,11 +36,31 @@ export default class SoundWrapper extends Component {
         });
     }
 
-    randomize () {
+    playSounds () {
+        this.props.songs.map((song, index) =>{
+            this.setState({
+                [song.title]: 0
+            });
+        })
         this.setState({
-            randomize: !this.state.randomize
+            isPlaying: true
         });
     }
+
+    handleVolume(volume){
+        this.props.songs.map((song) =>{
+            this.setState({
+                [song.title]: volume
+            });
+        })
+    }
+
+    handleChange = (event, newValue) => {
+        console.log("EVENT: " + event.target.id + "    " + "NEWVAL: " + newValue)
+        this.setState({
+            [event.target.id]: newValue
+        });
+    };
 
     mute(){
         this.setState({
@@ -38,18 +73,38 @@ export default class SoundWrapper extends Component {
         return (
             <div>
                 <button onClick={this.mute}>Mute</button>
-                <button onClick = {(this.randomize)}>Randomize</button>
+                <button onClick = {this.playSounds}>Play</button>
 
                 {
                     this.props.songs.map((song,index)=>{
+                        let volume = this.state[song.title]
                         return (
-                            <PlaySound 
-                                key={index}
-                                song={song}
-                                setIntitialPlay={this.state.setIntitialPlay}
-                                randomize={this.state.randomize}
-                                mute={this.state.mute}
-                            />
+                            <div>
+                                <div>
+                                    {song.title}
+                                </div>
+
+                                <Sound
+                                    url={song.path}
+                                    playStatus={this.state.isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
+                                    playFromPosition={10}
+                                    volume={this.state[song.title]}
+                                    loop = {true}
+                                    //autoLoad={true}
+                                />
+
+                                <Grid container spacing={2}>
+                                    <Grid item>
+                                        <VolumeDown />
+                                    </Grid>
+                                    <Grid item xs>
+                                        <Slider id={song.title} value={this.state[song.title]} onChange={this.handleChange} aria-labelledby="continuous-slider" />
+                                    </Grid>
+                                    <Grid item>
+                                        <VolumeUp />
+                                    </Grid>
+                                </Grid>
+                            </div>
                         )
                     })
                 }
